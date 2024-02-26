dd09a509c389a2de77935409192fbc7466878f87
Hjmontalban <99377761+Hjmontalban@users.noreply.github.com>
2024-02-26 20:36:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,18 @@
-# Input the relevant libraries
 import numpy as np
 import pandas as pd
 import streamlit as st
-import altair as alt
 import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
 
 # Define the Streamlit app
 def app():
-    
     st.title('Symbol Classification')
     st.subheader('Humphrey John M. Montalban.,BSCS 3B AI')
     
-    st.write('Dataset description:'
-             'A symbol dataset is usually made up of a grouping of characters or symbols that are frequently arranged in an ordered manner. These symbols may consist of special characters, numerals, letters, and punctuation. Each symbol\'s Unicode code point, representation in different fonts or styles, usage frequency, and linguistic characteristics are just a few examples of the metadata or annotations that could be included in the dataset.')
+    st.write('Dataset description: A symbol dataset is usually made up of a grouping of characters or symbols that are frequently arranged in an ordered manner. These symbols may consist of special characters, numerals, letters, and punctuation. Each symbol\'s Unicode code point, representation in different fonts or styles, usage frequency, and linguistic characteristics are just a few examples of the metadata or annotations that could be included in the dataset.')
 
     st.write('Number of features: 64')
     text = """Feature representation: Binary values (1 or 0) representing the 8x8 pixels of an image.
@@ -36,7 +30,7 @@ def app():
     clf = BernoulliNB() 
     options = ['Naive Bayes', 'Logistic Regression']
     selected_option = st.selectbox('Select the classifier', options)
-    if selected_option=='Logistic Regression':
+    if selected_option == 'Logistic Regression':
         clf = LogisticRegression(C=100, max_iter=100, multi_class='auto',
             penalty='l2', random_state=42, solver='lbfgs',
             verbose=0, warm_start=False)
@@ -44,15 +38,15 @@ def app():
         clf = BernoulliNB()
 
     if st.button('Start'):
-       df_mint = pd.read_csv('Montalban Feb 17,2024 Symbols.csv')
+        df_mint = pd.read_csv('Montalban Feb 17,2024 Symbols.csv')
 
         # display the dataset
         st.header("Dataset")
-        st.dataframe(df, use_container_width=True) 
+        st.dataframe(df_mint, use_container_width=True) 
 
         # load the data and the labels
-        X = df.values[:,0:-1]
-        y = df.values[:,-1]    
+        X = df_mint.iloc[:, :-1].values
+        y = df_mint.iloc[:, -1].values    
 
         st.header('Images')
         # display the images 
@@ -76,7 +70,7 @@ def app():
         # Split the dataset into training and testing sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        clf.fit(X_train,y_train)
+        clf.fit(X_train, y_train)
         y_test_pred = clf.predict(X_test)
 
         st.header('Confusion Matrix')
@@ -85,7 +79,7 @@ def app():
         st.header('Classification Report')
         # Test the classifier on the testing set
         st.text(classification_report(y_test, y_test_pred))
-    
+
 # Run the app
 if __name__ == "__main__":
     app()
